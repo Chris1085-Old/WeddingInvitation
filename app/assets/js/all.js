8daf5a46cd01b84e0abb7f3cd867883ef11a5caf
chris <itscfchiang@gmail.com>
2021-10-15 17:09:55 +0800

    git init

---
+++ app/assets/js/all.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+  console.log('Hello!')
+})
+
+// var promise = document.querySelector('video').play()
+
+// if (promise !== undefined) {
+//   promise
+//     .then((_) => {
+//       // Autoplay started!
+//     })
+//     .catch((error) => {
+//       // Autoplay was prevented.
+//       // Show a "Play" button so that user can start playback.
+//     })
+// }

8e921e2fead694fe5a437c8e534944403e50fe80
chris1085 <itscfchiang@gmail.com>
2021-11-14 22:00:46 +0800

    feat: Add location and wedding info section RWD

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,1 +1,12 @@
-$(document).ready(() => {})
+$(document).ready(() => {
+  const fixTop = document.getElementById('fixTop')
+  $(window).scroll(function() {
+    let scroll_top = $(this).scrollTop()
+
+    if (scroll_top >= 360) {
+      fixTop.classList.add('fixTop')
+    } else {
+      fixTop.classList.remove('fixTop')
+    }
+  })
+})
